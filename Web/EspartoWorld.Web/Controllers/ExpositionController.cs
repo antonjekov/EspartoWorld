cea447696f5dbdd15a23fd66b46802449e2b18c5
Anton Jekov <antonjekov@gmail.com>
2020-11-01 12:11:07 +0100

    Try to flip cards

---
+++ Web/EspartoWorld.Web/Controllers/ExpositionController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EspartoWorld.Web.Controllers
+{
+    public class ExpositionController : BaseController
+    {
+        public IActionResult Add()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAsync()
+        {
+            //var id = await this.coursesService.AddAsync(input);
+            return this.Redirect("/");
+        }
+
+        public IActionResult All()
+        {
+            //var courses = this.coursesService.GetAll<CourseViewModel>();
+            return this.View();
+        }
+    }
+}
